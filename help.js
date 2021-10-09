45ca995f77733bd8a84f616e6cc975329ee53b99
YOROSBOT <92189398+YOROSBOT@users.noreply.github.com>
2021-10-09 08:24:03 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,50 @@
+const help = (prefix) => {
+	return `
+
+
+BOT CRIADO PELO ✧AӃ✫YOROS 𓅂
+
+FIGURINHAS: 
+
+*${prefix}sticker* ou *${prefix}stiker*
+Envie ou responder imagem/gif/vídeo com legenda a *${prefix}sticker*\n 
+*${prefix}toimg*
+Responda a figurinha com *${prefix}toimg*\n 
+
+       GRUPOS 
+
+- O bot Yoros tem quer ser ADM do grupo. 
+
+*${prefix}welcome [1/0]*
+Desc: Ativa o mode de boas vinda para o grupo 
+
+*${prefix}add*
+*${prefix} add 62813xxxxx* \n 
+
+*${prefix}kick*
+*${prefix} kick @[membro do grupo]* \n 
+
+*${prefix}promote*
+*${prefix}promote @[numero do membro]* \n 
+
+*${prefix}demote*
+(*${prefix}demote @[mumero do membro]* \n 
+
+*${prefix}linkgroup*
+( Basta enviar o comando 
+
+*${prefix}leave*
+Use este comando para o bot sair do grupo 
+
+*${prefix}tagall*
+Desc: Marca todos os membros do grupo, inclusive administradores 
+
+*${prefix}tagall2*
+Desc: Marca todos os membros do grupo, inclusive administradores 
+
+*${prefix}tagall3*
+Marca todos os membros do grupo, inclusive administradores 
+`
+} 
+
+exports.help = help
